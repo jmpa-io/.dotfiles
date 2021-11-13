8fdcaa25bc6a822b4448a1d693fcf43d27a2a0ff
Jordan Cleal <mr.jordan.cleal@gmail.com>
2021-11-13 22:49:51 +1100

    flesh out a more in-depth dotfiles repo

---
+++ README.md
@@ -0,0 +1,9 @@
+<p align="center">
+  <img src="./img/logo.png">
+</p>
+
+# .dotfiles
+
+```diff
++ Here are my dotfiles.
+```
---
+++ setup.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+# installs dotfiles & largely sets up a new PC.
+# note: fonts should be installed manually.
+
+# funcs.
+die() { echo "$1" >&2; exit "${2:-1}"; }
+
+# check bash version.
+v=5; [[ ${BASH_VERSION:0:1} -lt "$v" ]] \
+    && die "bash version $v required, please update"
+
+# check root.
+[[ -d .git ]] \
+    || die "$0 must be run from the repository root directory"
+
+# check deps.
+deps=("curl")
+for dep in "${deps[@]}"; do
+    hash "$dep" 2>/dev/null || missing+=("$dep")
+done
+if [[ ${#missing[@]} -ne 0 ]]; then
+    [[ ${#missing[@]} -gt 1 ]] && { s="s"; }
+    die "missing dep${s}: ${missing[*]}"
+fi
+
+# check os.
+# TODO support darwin.
+os=$(uname) \
+    || die "failed to get operating system"
+[[ $os == [Ll]inux ]] \
+    || die "$os is not supported, missing implementation"
+
+# setup cleanup.
+cleanup() {
+    files=("./starship.sh")
+    for file in ${files[@]}; do
+        [[ -f "$file" ]] || { continue; }
+        rm -rf "$file" \
+            || die "failed to clean up $file"
+    done
+}
+trap cleanup SIGTERM SIGINT EXIT
+
+# install zsh, if not installed.
+if ! [[ -f /usr/bin/zsh ]]; then
+
+    # install zsh.
+    # TODO what if not running ubuntu / missing apt-get?
+    sudo apt-get install -y zsh \
+        || die "failed to install zsh"
+
+    # switch default shell.
+    chsh -s $(which zsh) \
+        || die "failed to change default shell to zsh"
+fi
+
+# install starship, if not installed.
+if [[ $(starship -V &>/dev/null) -ne 0 ]]; then
+
+    # download starship.
+    file=starship.sh
+    curl -sSL -o "$file" https://starship.rs/install.sh  \
+        || die "failed to download $file"
+    chmod +x ./$file \
+        || die "failed to grant execute permissions to $file"
+
+    # install starship.
+    ./$file -V -y \
+        || die "failed to install $file"
+
+    # install config.
+    dir="$HOME/.config"
+    if ! [[ -d "$dir" ]]; then
+        mkdir "$dir" \
+            || die "failed to create $dir"
+    fi
+    ln -s "$PWD/starship/starship.toml" "$dir/starship.toml" \
+        || die "failed to create symlink for starship config."
+fi
+
+# setup directories.
+dirs=("$HOME/.local" )
+for dir in "${dirs[@]}"; do
+    [[ -d "$dir" ]] && { continue; }
+    mkdir "$dir" \
+        || die "failed to create $dir"
+done
+
+# setup .dotfiles in $HOME, if needed.
+if ! [[ -d "$HOME/.dotfiles" ]]; then
+    ln -s "$PWD" "$HOME/.dotfiles" \
+        || die "failed to create symlink for .dotfiles"
+fi
+
+# setup .zshrc files, if needed.
+files=("zshenv" "zshrc")
+for file in "${files[@]}"; do
+    src="$PWD/zsh/$file"
+    dest="$HOME/.$file"
+    if ! [[ -L "$dest" ]]; then
+        ln -s "$src" "$dest" \
+            || die "failed to create symlink from $src to $dest"
+    fi
+done
+
+# install go.
+if [[ $(go version &>/dev/null) -ne 0 ]]; then
+    sudo apt-get install golang -y \
+        || die "failed to install go"
+fi
+
+# install gitconfig.
+if ! [[ -f "$HOME/.gitconfig" ]]; then
+    ln -s "$PWD/git/gitconfig" "$HOME/.gitconfig" \
+        || die "failed to create symlink for gitconfig"
+fi
