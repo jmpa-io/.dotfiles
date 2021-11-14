e231d23e9df245b0c9b638f9c80e7fe39fea10e9
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2021-11-14 14:00:18 +1100

    add microsoft-terminal settings

--- microsoft-terminal/install.bat
+++ microsoft-terminal/install.bat
@@ -1,2 +1,3 @@
+:: PLEASE NOTE: this must be run manually outside of WSL.
 :: context: https://github.com/microsoft/terminal/issues/1812
-mklink /h %LocalAppData%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json %USERPROFILE%\MEGA\configs\"Microsoft Terminal"\settings.json
+mklink /h %LocalAppData%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json D:\MEGA\.dotfiles\microsoft-terminal\settings.json
