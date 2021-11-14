ca48306c96e5beb20c34a5881fcec756752f8a9e
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2021-11-14 13:36:26 +1100

    add microsoft-terminal settings

---
+++ microsoft-terminal/install.bat
@@ -0,0 +1,2 @@
+:: context: https://github.com/microsoft/terminal/issues/1812
+mklink /h %LocalAppData%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json %USERPROFILE%\MEGA\configs\"Microsoft Terminal"\settings.json
