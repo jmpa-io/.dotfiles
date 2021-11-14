2a6962c6a1ad22c922be71f773a9bb4d69b575bf
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2021-11-14 14:29:09 +1100

    fix up symlink for windows-terminal

--- microsoft-terminal/install.bat
+++ microsoft-terminal/install.bat
@@ -1,3 +1,4 @@
-:: PLEASE NOTE: this must be run manually outside of WSL.
-:: context: https://github.com/microsoft/terminal/issues/1812
-mklink /h %LocalAppData%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json D:\MEGA\.dotfiles\microsoft-terminal\settings.json
+:: PLEASE NOTE: this must be run manually outside of WSL, must be run when the 'Microsoft Terminal' is not running.
+:: CONTEXT: https://github.com/microsoft/terminal/issues/1812
+del %LocalAppData%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+mklink /h %LocalAppData%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json %UserProfile%\Documents\MEGA\.dotfiles\microsoft-terminal\settings.json
